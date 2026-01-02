135a89d879fbad619f5f80b7c4f69d138c31bf46
mayank <mayank.mehra.dev@gmail.com>
2026-01-02 18:44:39 +0530

    Implemented the update api for diesel issued

--- app/router/issued_router.py
+++ app/router/issued_router.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.controller.diesel_controller import create_diesel_issued_entry, get_diesel_issued_entry, \
-    get_all_diesel_issued_entries, delete_diesel_issued_entry
+    get_all_diesel_issued_entries, delete_diesel_issued_entry,update_diesel_issued_entry
 from app.database.dbConfig import get_db
 from app.schemas.api_schema import APIResponse
-from app.schemas.diesel_schema import RequestSchema_DieselIssued_Create, ResponseSchema_DieselIssued_Create
+from app.schemas.diesel_schema import RequestSchema_DieselIssued_Create, ResponseSchema_DieselIssued_Create, \
+    RequestSchema_DieselIssued_Update
 
 router = APIRouter(prefix="/issued", tags=["Assets Issued"])
 
@@ -48,6 +49,7 @@ async def get_all_diesel_entries(db: Session = Depends(get_db)):
         data=response_data
     )
 
+
 @router.delete("/diesel/{recordId}", response_model=APIResponse)
 async def delete_diesel_entry(recordId: str, db: Session = Depends(get_db)):
     returned_data = await delete_diesel_issued_entry(recordId, db)
@@ -55,4 +57,15 @@ async def delete_diesel_entry(recordId: str, db: Session = Depends(get_db)):
         message=f"Data with the id {recordId} was deleted",
         status="success",
         data=returned_data
+    )
+
+
+@router.patch("/diesel/{recordId}", response_model=APIResponse)
+async def update_diesel_entry(data: RequestSchema_DieselIssued_Update, recordId: str, db: Session = Depends(get_db)):
+    updated_data = await update_diesel_issued_entry(recordId, db, data)
+    response_data = ResponseSchema_DieselIssued_Create.model_validate(updated_data)
+    return APIResponse(
+        message=f"Data with the id {recordId} was updated",
+        status="success",
+        data=response_data
     )
